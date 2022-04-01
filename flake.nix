58801e3dd939e2053f14528216c33181945f8d9d
Tom Bereknyei <tomberek@gmail.com>
2022-04-01 15:01:02 -0400

    ci: move to capacitor

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:flox/nixpkgs/staging";
-  inputs.capacitor.url = "path:/home/tom/flox/capacitor";
+  inputs.capacitor.url = "git+ssh://git@github.com/flox/capacitor";
 
   outputs = self: {
     apps = self.capacitor.lib.makeApps self.nixpkgs;
