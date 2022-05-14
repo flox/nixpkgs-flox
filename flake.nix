23714ca2e6f18752790c9a700ff303106ef134f7
Tom Bereknyei <tomberek@gmail.com>
2022-05-14 16:00:15 -0400

    fix: no top-level nixpkgs

--- flake.nix
+++ flake.nix
@@ -9,52 +9,63 @@ rec {
 
   inputs.nix-eval-jobs.url = "github:tomberek/nix-eval-jobs";
 
-  outputs = args@{self, nixpkgs, capacitor,nix-eval-jobs,...}:
-  {lib = nixpkgs.lib;} //
-  capacitor args (_: {
-    legacyPackages = {system,...}: let pkgs = (nixpkgs.lib.genAttrs ["stable" "staging" "unstable"] (stability: import args."nixpkgs-${stability}" {
-      config.allowUnfree = true;
-      inherit system;
-    })); in (import args.nixpkgs {
-      config.allowUnfree = true;
-      inherit system;
-    })// pkgs;
+  outputs = args @ {
+    self,
+    nixpkgs,
+    capacitor,
+    nix-eval-jobs,
+    ...
+  }:
+    {lib = nixpkgs.lib;}
+    // capacitor args (_: {
+      legacyPackages = {system, ...}: let
+        pkgs = nixpkgs.lib.genAttrs ["stable" "staging" "unstable"] (
+          stability:
+            (import args."nixpkgs-${stability}" {
+              config.allowUnfree = true;
+              inherit system;
+            })
+            // {recurseForDerivations = true;}
+        );
+      in pkgs
+        // {recurseForDerivations = true;};
 
-    packages.full-eval = {system,...}: let
-          system = "x86_64-linux";
-    in with nixpkgs.legacyPackages.${system};
-            runCommand "full-eval" {
-              nativeBuildInputs = [
-                nixUnstable
-                jq
-                nix-eval-jobs.defaultPackage.${system}
-              ];
-              allowedReferences = [];
-            } ''
-              export HOME=$PWD
-              export GC_DONT_GC=1
-              export NIX_CONFIG="experimental-features = flakes nix-command
-              store = $PWD/temp
-              "
-              mkdir temp gc self
-              nix-eval-jobs --gc-roots-dir $PWD/gc \
-                --flake ${nixpkgs}#legacyPackages.${system}.perlPackages \
-                --depth 1 \
-                | jq -c '.originalUri = "${inputs.nixpkgs.url}" |
-                         .uri = "${builtins.dirOf inputs.nixpkgs.url}/${nixpkgs.rev}"' \
-                | tee /dev/fd/2 | \
-                  jq -sf ${capacitor}/lib/split.jq > self/pkgs.json
-              cp ${./template.nix} self/flake.nix
-              cat > self/flake.lock <<EOF
-              {
-                "nodes": {
-                  "root": {}
-                },
-                "root": "root",
-                "version": 7
-              }
-              EOF
-              tar -acf $out self
-            '';
-  });
+      packages.full-eval = {system, ...}: let
+        system = "x86_64-linux";
+      in
+        with nixpkgs.legacyPackages.${system};
+          runCommand "full-eval" {
+            nativeBuildInputs = [
+              nixUnstable
+              jq
+              nix-eval-jobs.defaultPackage.${system}
+            ];
+            allowedReferences = [];
+          } ''
+            export HOME=$PWD
+            export GC_DONT_GC=1
+            export NIX_CONFIG="experimental-features = flakes nix-command
+            store = $PWD/temp
+            "
+            mkdir temp gc self
+            nix-eval-jobs --gc-roots-dir $PWD/gc \
+              --flake ${nixpkgs}#legacyPackages.${system}.perlPackages \
+              --depth 1 \
+              | jq -c '.originalUri = "${inputs.nixpkgs.url}" |
+                       .uri = "${builtins.dirOf inputs.nixpkgs.url}/${nixpkgs.rev}"' \
+              | tee /dev/fd/2 | \
+                jq -sf ${capacitor}/lib/split.jq > self/pkgs.json
+            cp ${./template.nix} self/flake.nix
+            cat > self/flake.lock <<EOF
+            {
+              "nodes": {
+                "root": {}
+              },
+              "root": "root",
+              "version": 7
+            }
+            EOF
+            tar -acf $out self
+          '';
+    });
 }
