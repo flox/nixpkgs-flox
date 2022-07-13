0e7c217aadf9999e2948fdc65ba592e05631959c
Tom Bereknyei <tomberek@gmail.com>
2022-07-13 09:44:19 -0400

    fix: reorganize full-eval

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-{
+rec {
   inputs.nixpkgs.url = "github:flox/nixpkgs/unstable";
 
   inputs.nixpkgs-stable.url = "github:flox/nixpkgs/stable";
@@ -14,31 +14,32 @@
     nix-eval-jobs,
     nix-editor,
     ...
-  }:
-  ({
-  __systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
-      legacyPackages = nixpkgs.lib.genAttrs self.__systems (system: let
-        pkgs = nixpkgs.lib.genAttrs ["stable" "staging" "unstable"] (
-          stability:
-            (import args."nixpkgs-${stability}" {
-              config.allowUnfree = true;
-              inherit system;
-            })
-            // {recurseForDerivations = true; nix-editor = args.nix-editor.packages.${system}.nixeditor;}
-        );
-      in pkgs
-        // pkgs.unstable
-        // {recurseForDerivations = true;});
+  }: {
+    __systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    legacyPackages = nixpkgs.lib.genAttrs self.__systems (system: let
+      pkgs = nixpkgs.lib.genAttrs ["stable" "staging" "unstable"] (
+        stability:
+          (import args."nixpkgs-${stability}" {
+            config.allowUnfree = true;
+            inherit system;
+          })
+          // {
+            recurseForDerivations = true;
+            nix-editor = args.nix-editor.packages.${system}.nixeditor;
+          }
+      );
+    in
+      pkgs
+      // pkgs.unstable
+      // {recurseForDerivations = true;});
 
-      stable.legacyPackages = builtins.mapAttrs (_: v: v.stable) self.legacyPackages;
-      unstable.legacyPackages = builtins.mapAttrs (_: v: v.unstable) self.legacyPackages;
-      staging.legacyPackages = builtins.mapAttrs (_: v: v.staging) self.legacyPackages;
-
-      packages.full-eval = {system, ...}: let
-        system = "x86_64-linux";
-      in
-        with nixpkgs.legacyPackages.${system};
-          runCommand "full-eval" {
+    stable.legacyPackages = builtins.mapAttrs (_: v: v.stable) self.legacyPackages;
+    unstable.legacyPackages = builtins.mapAttrs (_: v: v.unstable) self.legacyPackages;
+    staging.legacyPackages = builtins.mapAttrs (_: v: v.staging) self.legacyPackages;
+    packages = builtins.mapAttrs (system: pkgs:
+      with pkgs; {
+        full-eval =
+          runCommand "full-eval.tar.gz" {
             nativeBuildInputs = [
               nixUnstable
               jq
@@ -53,12 +54,12 @@
             "
             mkdir temp gc self
             nix-eval-jobs --gc-roots-dir $PWD/gc \
-              --flake ${nixpkgs}#legacyPackages.${system}.perlPackages \
+              --flake ${nixpkgs}#legacyPackages.${system} \
               --depth 1 \
               | jq -c '.originalUri = "${inputs.nixpkgs.url}" |
                        .uri = "${builtins.dirOf inputs.nixpkgs.url}/${nixpkgs.rev}"' \
               | tee /dev/fd/2 | \
-                jq -sf ${capacitor}/lib/split.jq > self/pkgs.json
+                jq -sf ${./split.jq} > self/pkgs.json
             cp ${./template.nix} self/flake.nix
             cat > self/flake.lock <<EOF
             {
@@ -71,7 +72,9 @@
             EOF
             tar -acf $out self
           '';
-        }) //
-        {lib = args.nixpkgs-unstable.lib;}
-        ;
+      }) nixpkgs.legacyPackages;
+
+    lib = args.nixpkgs-unstable.lib;
+    stabilities = {inherit (self) stable unstable staging;};
+  };
 }
